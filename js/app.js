4c1154a368bc0446c45fa27d0b61db5c500e3806
josemiguelhen <107695767+josemiguelhen@users.noreply.github.com>
2024-11-10 05:35:52 -0300

    se incluye variables y funciones en app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
 //Variables
+const carrito = document.querySelector('#carrito');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
+const listaCursos = document.querySelector('#lista-cursos');
+
+cargarEventListeners();
+function cargarEventListeners(){
+    //Cuandro agregas un curso presionando "Agregar al carrito"
+    listaCursos.addEventListener('click', agregarCurso);
+}
+
+
+//Funciones
+function agregarCurso(e) {
+    e.preventDefault();
+
+
+    if(e.target.classList.contains('agregar-carrito')){
+       console.log(e.target)
+    }   
+
+
+}
